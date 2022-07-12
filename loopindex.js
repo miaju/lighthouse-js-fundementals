d35a13ef74decc289b88c4a87cebd11d9a0287aa
Mia Tanner <mia@indygecko.com>
2022-07-12 14:57:03 -0700

    looping - lastIndexOf assignment submission

---
+++ loopindex.js
@@ -0,0 +1,18 @@
+const lastIndexOf = function(array, num){
+  let index = -1;
+  let counter = 0;
+  for(const a of array){
+    if(a === num){
+      index = counter;
+    }
+    counter++;
+  } 
+
+  return index;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);

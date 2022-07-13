8fc6c81dcf4b8ecfea7647d4abea9028f67b3db1
Mia Tanner <mia@indygecko.com>
2022-07-12 19:26:42 -0700

    driving miss daisy assignment submission

---
+++ car.js
@@ -0,0 +1,25 @@
+const carPassing = function(cars, speed){
+  let result = cars;
+  let time = Date.now();
+  result.push( { time: time, speed: speed } );
+  return result;
+};
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+console.log(carPassing(cars, speed))

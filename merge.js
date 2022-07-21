1b3125906ed7ea304924e8f424daaaefa18b39fd
Mia Tanner <mia@indygecko.com>
2022-07-21 12:01:46 -0700

    merge stretch assignment submission

---
+++ merge.js
@@ -0,0 +1,10 @@
+const merge = function(arr1, arr2){
+  let result = arr1.concat(arr2);
+  result.sort(function(a, b){ return a - b });
+  return result;
+}
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
